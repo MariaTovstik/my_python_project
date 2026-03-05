f475b07b5cb84502a0369326de2c99e93ce6848a
LizaFoxXx <E.Bugareva@yandex.ru>
2026-03-05 11:50:12 +0300

    Fest: add Rich

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from rich import print
 import conflict
 from utils import get_greeting
 
 def main():
-	print(f"Welcome to {config.APP_NAME}")
+	print(f"[yellow]Welcome to {config.APP_NAME}[/yellow]")
 	user_message = get_greeting("Student")
 	print(user message)
 	print("Project version 1.1")
